676d284f8a4862f3f0c7ef154b8c71d52e9351f7
Anastassy Cap <anacap100@gmail.com>
2022-10-15 09:02:43 -0400

    scattering input sequence to all child processes

--- Comp_428_QuickSort/QuickSort_Main.cpp
+++ Comp_428_QuickSort/QuickSort_Main.cpp
@@ -5,18 +5,6 @@
 
 #define MASTER 0        // task ID of master task
 
-std::vector<int> generateUnsortedSequence( int sequenceSize )
-{
-    std::vector<int> sequence(sequenceSize);
-
-    for (int i = 0; i < sequenceSize; ++i)
-    {
-        sequence[i] = std::rand(); // random number between 0 and RAND_MAX
-    }
-
-    return sequence;
-}
-
 int main(int argc, char* argv[])
 {
     if (argc != 2)
@@ -34,12 +22,6 @@ int main(int argc, char* argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &taskId);
     std::cout << "MPI task " << taskId << " has started...\n";
-
-    // set the seed for the random number generator
-    srand(time(NULL) + taskId);
-
-    // Generate an unsorted, random sequence
-    std::vector<int> sequence = generateUnsortedSequence( 100 );
 
     // The master task will dynamically spawn all the other tasks
     const int nbrOfProcessesToStart = std::stoi(argv[1]);
@@ -61,43 +43,7 @@ int main(int argc, char* argv[])
     MPI_Comm childComm;
     MPI_Comm_spawn("c", argv, nbrOfProcessesToStart, MPI_INFO_NULL, MASTER, MPI_COMM_SELF, &childComm, MPI_ERRCODES_IGNORE);
 
-    /*	
-        MPI_Scatter(
-            void* send_data,
-            int send_count,
-            MPI_Datatype send_datatype,
-            void* recv_data,
-            int recv_count,
-            MPI_Datatype recv_datatype,
-            int root,
-            MPI_Comm communicator
-            )
-    */
-
-    // Chunk size that each process will receive
-    // int chunkSizeCount = sequence.size() / nbrOfProcessesToStart;
-
-    // Send the size of the sequence first and then the array
-    int sequenceSize = sequence.size();
-    MPI_Send((void*)&sequenceSize, 1, MPI_INT, 0, 0, childComm);
-
-    // Send the entire array to the first child process which will be the main process that selects the pivots for the parallel quicksort algorithm
-    MPI_Send((void*)&sequence[0], sequence.size(), MPI_INT, 0, 0, childComm);
-
-    // print the sequence
-    std::cout << "Sequence from master: \n";
-    for (int i = 0; i < sequence.size(); i++) {
-        std::cout << sequence[i] << ", ";
-    }
-    std::cout << "\n";
-
-    //std::vector<int> recvArr(chunkSizeCount);
-    //MPI_Scatter(&sequence, chunkSizeCount, MPI_INT, &recvArr, chunkSizeCount, MPI_INT, MASTER, childComm);
-
-    //// Each Process then print their received chunks
-    //for (int i = 0; i < chunkSizeCount; i++) {
-    //    std::cout << "\nP" << taskId << ": " << recvArr[i] << "\n";
-    //}
+    // Wait for the children processes to finish?
 
     // Finalize the MPI environment.
     MPI_Finalize();
--- Comp_428_QuickSort/child.cpp
+++ Comp_428_QuickSort/child.cpp
@@ -2,6 +2,32 @@
 #include <vector>
 #include <mpi.h>
 
+#define CHUNK_SIZE 100
+
+void recursiveHalving(int taskId, int numTasks);
+
+void printVectorContents( std::vector<int> sequence, int taskId )
+{
+    // Print the received sequence
+    std::cout << "Child #" << taskId << ":\n";
+    for (int i = 0; i < sequence.size(); i++) {
+        std::cout << sequence[i] << ", ";
+    }
+    std::cout << "\n";
+}
+
+std::vector<int> generateUnsortedSequence(int sequenceSize)
+{
+    std::vector<int> sequence(sequenceSize);
+
+    for (int i = 0; i < sequenceSize; ++i)
+    {
+        sequence[i] = std::rand(); // random number between 0 and RAND_MAX
+    }
+
+    return sequence;
+}
+
 int main(int argc, char* argv[])
 {
     int	taskId;	     // task ID - also used as seed number
@@ -13,9 +39,11 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &taskId);
 
     MPI_Comm parentComm;
-    std::cout << "Child getting parent communicator..." << std::endl;
     MPI_Comm_get_parent(&parentComm);
     MPI_Status receiveStatus;
+
+    int sequenceSize = numTasks * CHUNK_SIZE;
+    std::vector<int> sequence;
 
     if (taskId == 0)
     {
@@ -24,26 +52,40 @@ int main(int argc, char* argv[])
         // The master task in this child world will be in charge of selecting the pivots in the quicksort algorithm
         // Before doing that, we must first receive the entire input sequence for the parallel quicksort algorithm
 
-        // First, we will receive the size of the input sequence that we must sort and then the actual sequence
-        int sequenceSize;
-        MPI_Recv((void*)&sequenceSize, 1, MPI_INT, 0, 0, parentComm, &receiveStatus);
-        std::cout << "Child process received sequence size: " << sequenceSize << "\n";
+        // set the seed for the random number generator
+        srand(time(NULL) + taskId);
 
-        // Now we will receive the actual sequence
-        std::vector<int> sequence(sequenceSize);
-        MPI_Recv((void*)&sequence[0], sequenceSize, MPI_INT, 0, 0, parentComm, &receiveStatus);
-        std::cout << "Child process received sequence \n";
-
-        // Print the received sequence
-        std::cout << "Child #" << taskId << ":\n";
-        for (int i = 0; i < sequence.size(); i++) {
-            std::cout << sequence[i] << ", ";
-        }
-        std::cout << "\n";
+        // Generate an unsorted, random sequence
+        sequence = generateUnsortedSequence(sequenceSize);
     }
 
-    std::cout << "\nChild task #" << taskId << " is terminating..." << std::endl;
+    // The next step is to scatter the input sequence among all the children in this world
+    // It is important to place this call outside the if statement because it is a collective
+    /*	
+        MPI_Scatter(
+        void* send_data,
+        int send_count,
+        MPI_Datatype send_datatype,
+        void* recv_data,
+        int recv_count,
+        MPI_Datatype recv_datatype,
+        int root,
+        MPI_Comm communicator)
+    */
+    std::vector<int> subarray( CHUNK_SIZE );
+    MPI_Scatter( sequence.data(), CHUNK_SIZE, MPI_INT, subarray.data(), CHUNK_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+
+    //Each Process then print their received chunks
+    std::cout << "\nChild #" << taskId << " received subarray of size " << CHUNK_SIZE << std::endl;
+    printVectorContents(subarray, taskId);
+
+    std::cout << "\nChild #" << taskId << " is terminating..." << std::endl;
     MPI_Finalize();
 
     return 0;
+}
+
+void recursiveHalving(int taskId, int numTasks)
+{
+   
 }
